96ce2d21449c7ef7a1a28e1bc59311284685a5b8
billmoriarty <wrmoriarty@gmail.com>
2020-03-10 21:05:41 -0400

    Initial Commit

---
+++ TrackPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrackPractice
+//
+//  Created by Bill Moriarty on 3/10/20.
+//  Copyright © 2020 Bill Moriarty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
